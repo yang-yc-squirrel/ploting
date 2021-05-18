87f36db6fcfbfbf90916c95c899614010354b0b2
yang-yc-squirrel <2109270989@qq.com>
2021-05-18 16:50:30 +0800

    test

--- DataAnalysis/test3.py
+++ DataAnalysis/test3.py
@@ -1,2 +1,13 @@
-a=12*15*24
+def Hammingcode(m):
+    k=1
+    while(True):
+        ri=pow(2,k)
+        le=m+1+k
+        if(ri>=le):
+            res=k
+            break
+        k+=1
+    return res
+
+a=Hammingcode(16)
 print(a)
