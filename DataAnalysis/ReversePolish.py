ab851bae609f492095d92b8916c6ed7c4a1d81c9
yang-yc-squirrel <2109270989@qq.com>
2021-05-19 15:59:39 +0800

    transform

--- DataAnalysis/ReversePolish.py
+++ DataAnalysis/ReversePolish.py
@@ -1,5 +1,23 @@
 def repolish(s):
+    numlist=list(range(0,10,1))
+    symlist=[chr(ord("a")+i) for i in range(26)]+[chr(ord("A")+j) for j in range(26)]
+    opelist=["+","-","*","/","(",")"]
+    stack1=[]
+    stack2=[]
     li=list(s)
+    for k in li:
+        if(k in numlist or k in symlist):
+            stack2.append(k)
+        else:
+            if(k in opelist):
+                if(len(stack1)==0):
+                    stack1.append(k)
+                else:
+                    finalope=stack1[len(stack1)-1]
+                    
+
+
+
     return li
 
 s=input("please input an expression:")
