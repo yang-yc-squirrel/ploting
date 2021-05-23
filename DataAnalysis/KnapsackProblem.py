62cdd1f5e1082dac2368b8367938bc77c36fc1fd
yang-yc-squirrel <2109270989@qq.com>
2021-05-23 19:13:26 +0800

    knapsack

--- DataAnalysis/KnapsackProblem.py
+++ DataAnalysis/KnapsackProblem.py
@@ -1,4 +1,12 @@
 import numpy
-def knapsack_plan(val_li,wei_li,nums,volume):
-    ans=numpy.zeros(shape=(nums,volume))
+def knapsack_init(val_li,wei_li,nums,volume):
+    ans = numpy.zeros(shape=(nums+1, volume+1))
+    for i in range(0,nums+1,1):
+        for j in range(0,volume+1,1):
+            ans[i,j]=-1
+    return ans
+def knapsack_plan(val_li,wei_li,i,j,ans):
+    if(ans[i,j]!=-1):
+        return ans[i,j]
     
+
