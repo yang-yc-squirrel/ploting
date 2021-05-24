9cc080fd3a680bef33e9b233df3affb70e64b2a0
yang-yc-squirrel <2109270989@qq.com>
2021-05-24 16:23:35 +0800

    find circle

--- DataAnalysis/HamiltonianCircuit.py
+++ DataAnalysis/HamiltonianCircuit.py
@@ -1,7 +1,14 @@
 import numpy
 def fingdcircle(adjmat):
     n=adjmat.shape[0]
-    x=numpy.zeros(shape=(1,n))
+    x=numpy.zeros(shape=(1,n)).flatten()
+    visited=numpy.zeros(shape=(1,n)).flatten()
+    k=0
+    x[k]=0
+    visited[x[k]]=1
+    for k in range(1,n,1):
+        x[k]=x[k]+1
+        if()
 
 
 g=numpy.zeros(shape=(1,10)).flatten()
