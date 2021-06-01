7075c00a576d13bbde2f0f2694dfb569d66d2fe1
yang-yc-squirrel <2109270989@qq.com>
2021-06-01 19:08:40 +0800

    trangle

--- DataAnalysis/PascalTriangle.py
+++ DataAnalysis/PascalTriangle.py
@@ -1,3 +1,19 @@
+def PascalTriangleRow(n):
+    if(n==0):
+        res=[1]
+    else:
+        if(n==1):
+            res=[1,1]
+        else:
+            storages=[PascalTriangleRow(n-1)[i:i+2] for i in range(0,len(PascalTriangleRow(n-1))-1,1)]
+            res=[sum(k) for k in storages]
+            res.append(1)
+            res.insert(0,1)
+    return res
+
+
+
+
 while(True):
     try:
         n=int(input("please enter the number of rows:"))
@@ -6,3 +22,10 @@ while(True):
     else:
         print("Received n")
         break
+
+nlist=list(range(n))
+print("begin print".center(20,"-"))
+for i in nlist:
+    print(PascalTriangleRow(i))
+
+
