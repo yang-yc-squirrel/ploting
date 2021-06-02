e40fb50f9662cc0b0682b3593e13ad912b2ea561
yang-yc-squirrel <2109270989@qq.com>
2021-06-02 10:14:36 +0800

    queen

--- DataAnalysis/EightQueens.py
+++ DataAnalysis/EightQueens.py
@@ -1,9 +1,31 @@
-'''import operator
+import operator
 def conflict(pos):
     for i in range(0,len([pos])-1,1):
         for j in range(i+1,len(pos),1):
-            if(operator.eq(pos[i],pos[j]) or )'''
+            if(operator.eq(pos[i],pos[j]) or operator.eq(abs(i-j),abs(pos[i]-pos[j]))):
+                res=True
+                break
+    else:
+        res=False
+    return res
 
+def initpos(n):
+    pos=[0 for i in range(n)]
+    return pos
 
-a=-5
-print("{0}".format(abs(a)))
+solnums=0
+n=8
+pos=initpos(n)
+k=1
+while(True):
+    while(True):
+        if (conflict(pos[0:k + 1:1])):
+            pos[k] += 1
+            if (operator.ge(pos[k], n)):
+                pos[k] = 0
+                k -= 1
+                pos[k] += 1
+        else:
+            break
+    
+    k=k+1
