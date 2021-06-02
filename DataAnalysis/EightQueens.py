76c90c97976c94997d81e65b5a4f61b9ff256331
yang-yc-squirrel <2109270989@qq.com>
2021-06-02 09:33:54 +0800

    queen

---
+++ DataAnalysis/EightQueens.py
@@ -0,0 +1,9 @@
+'''import operator
+def conflict(pos):
+    for i in range(0,len([pos])-1,1):
+        for j in range(i+1,len(pos),1):
+            if(operator.eq(pos[i],pos[j]) or )'''
+
+
+a=-5
+print("{0}".format(abs(a)))
