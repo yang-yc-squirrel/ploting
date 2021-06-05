b0ba9ebbf9c507a2f21bac6d03a22ef5d675e17c
yang-yc-squirrel <2109270989@qq.com>
2021-06-05 17:10:18 +0800

    sinplot

--- DataAnalysis/SinMoving.py
+++ DataAnalysis/SinMoving.py
@@ -4,6 +4,14 @@ import matplotlib.animation
 
 
 f=matplotlib.pyplot.figure()
-ax=f.gca(projection="polar")
-ax.bar(numpy.pi,3,width=numpy.pi/3)
+x=numpy.linspace(0,2*numpy.pi,500)
+y=numpy.zeros(shape=(1,500)).flatten()
+for i in range(500):
+    y[i]=numpy.sin(x[i])
+a=f.gca()
+a.plot(x,y,label="y$_1$=sin(x)")
+matplotlib.pyplot.legend(loc=1)
+matplotlib.pyplot.xlabel("time",fontdict={"family":"Times New Roman","size":20})
+matplotlib.pyplot.annotate("y=sin(1)",xy=(1,0.3),xytext=(1,0.6),arrowprops=dict(facecolor='black', shrink=0.05))
+matplotlib.pyplot.grid(True)
 matplotlib.pyplot.show()
