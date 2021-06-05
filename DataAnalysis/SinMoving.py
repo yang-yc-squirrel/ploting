eb659e9eaefec38bccee532ba480d611d1e62357
yang-yc-squirrel <2109270989@qq.com>
2021-06-05 20:51:21 +0800

    sinplot

--- DataAnalysis/SinMoving.py
+++ DataAnalysis/SinMoving.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot
 import matplotlib.animation
 
 
+
 f=matplotlib.pyplot.figure()
 ax=matplotlib.pyplot.axes(xlim=(0,2*numpy.pi),ylim=(-1.5,1.5))
 b,=ax.plot([],[])
@@ -14,10 +15,10 @@ def init():
     return b,
 
 def newf(i):
-    x.append(i)
-    y.append(numpy.sin(i))
+    x=numpy.linspace(0,i,300)
+    y=numpy.sin(x)
     b.set_data(x,y)
     return b,
 
-m=matplotlib.animation.FuncAnimation(f,newf,frames=numpy.linspace(0,2*numpy.pi,100),init_func=init,interval=3,blit=True)
+m=matplotlib.animation.FuncAnimation(f,newf,frames=numpy.linspace(0,2*numpy.pi,100),init_func=init,blit=True,interval=30)
 matplotlib.pyplot.show()
