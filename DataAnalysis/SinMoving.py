421fd1576b8158dea51fde7ecaef8d8fd8e63347
yang-yc-squirrel <2109270989@qq.com>
2021-06-05 18:09:16 +0800

    sinplot

--- DataAnalysis/SinMoving.py
+++ DataAnalysis/SinMoving.py
@@ -1,17 +1,16 @@
 import numpy
+import random
 import matplotlib.pyplot
 import matplotlib.animation
 
 
 f=matplotlib.pyplot.figure()
-x=numpy.linspace(0,2*numpy.pi,500)
-y=numpy.zeros(shape=(1,500)).flatten()
-for i in range(500):
-    y[i]=numpy.sin(x[i])
-a=f.gca()
-a.plot(x,y,label="y$_1$=sin(x)")
-matplotlib.pyplot.legend(loc=1)
-matplotlib.pyplot.xlabel("time",fontdict={"family":"Times New Roman","size":20})
-matplotlib.pyplot.annotate("y=sin(1)",xy=(1,0.3),xytext=(1,0.6),arrowprops=dict(facecolor='black', shrink=0.05))
-matplotlib.pyplot.grid(True)
+x=numpy.random.randint(1,9,size=(3,50)).flatten()
+y=numpy.random.randint(1,9,size=(50,3)).flatten()
+matplotlib.pyplot.axis([0,10,0,10])
+for i in range(len(x)):
+    sizes=random.randint(10,30)
+    matplotlib.pyplot.scatter(x[i],y[i],s=sizes,color="#ff0000",alpha=0.3)
+
+matplotlib.pyplot.annotate("scatter",xy=(5,5),xytext=(7,5),arrowprops={"facecolor":"blue","shrink":0.08})
 matplotlib.pyplot.show()
