1ac937c368a8c4345bf2dbe5b266cdd14bf61257
yang-yc-squirrel <2109270989@qq.com>
2021-06-19 18:10:06 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -1,28 +1,7 @@
-a=[chr(ord("a")+i) for i in range(19)]
-print(a)
-a.pop(0)
-print(a)
-a.pop(2)
-print(a)
-d=dict([("a",5),("b",36),("c",25)])
-print(d)
-print(d.get("a"))
+import matplotlib.pyplot
 
+def f(x):
+    c=10
+    return c if x<=c else x
 
-print(divmod(459,27))
-
-
-g=list(range(20))
-print(g)
-print(g[0:7])
-k=slice(0,7)
-print(g[k])
-
-for i in range(0,100,7):
-    print(i)
-
-
-
-print(a)
-p="".join(a)
-print(type(p))
+print("{0} {1} {2}".format(f(3),f(88),f(12)))
