dc2c79b913fbb13b26ef9d30d5283ea380cf9e92
yang-yc-squirrel <2109270989@qq.com>
2021-06-19 18:16:01 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -1,7 +1,16 @@
 import matplotlib.pyplot
+import numpy
 
 def f(x):
     c=10
     return c if x<=c else x
 
-print("{0} {1} {2}".format(f(3),f(88),f(12)))
+x=numpy.linspace(0,20,200)
+y=numpy.zeros(shape=(1,200)).flatten()
+for i in range(200):
+    y[i]=f(x[i])
+
+p=matplotlib.pyplot.figure()
+matplotlib.pyplot.plot(x,y,label="function")
+matplotlib.pyplot.legend(loc=2)
+matplotlib.pyplot.show()
