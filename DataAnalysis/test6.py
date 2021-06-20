be1b4b9a921fb75a415bdf0d1aa058630607ae89
yang-yc-squirrel <2109270989@qq.com>
2021-06-20 16:26:27 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -1,16 +1,7 @@
 import matplotlib.pyplot
 import numpy
 
-def f(x):
-    c=10
-    return c if x<=c else x
-
-x=numpy.linspace(0,20,200)
-y=numpy.zeros(shape=(1,200)).flatten()
-for i in range(200):
-    y[i]=f(x[i])
-
-p=matplotlib.pyplot.figure()
-matplotlib.pyplot.plot(x,y,label="function")
-matplotlib.pyplot.legend(loc=2)
-matplotlib.pyplot.show()
+a=numpy.linspace(0,1,3600)
+print(a)
+print(a.shape)
+print(divmod(3600,300))
