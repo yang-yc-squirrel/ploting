1075ca01d4207ae9470659094931ccf9884b6690
yang-yc-squirrel <2109270989@qq.com>
2021-06-20 16:36:23 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -1,7 +1,18 @@
 import matplotlib.pyplot
 import numpy
+import pandas
 
-a=numpy.linspace(0,1,3600)
+d=dict(a=[1,2,3],b=[5,6,7])
+p=pandas.DataFrame(d,index=list(range(3)),columns=["b","a"])
+print(p)
+p=p.append([{"b":12,"a":108}],ignore_index=True)
+print(p)
+
+
+a=numpy.random.randint(2,15,size=(5,6))
 print(a)
-print(a.shape)
-print(divmod(3600,300))
+k=a.sum(axis=0)
+print(k)
+
+p1=pandas.DataFrame(a,columns=list("abcdef"))
+print(p1)
