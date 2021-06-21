b4c07ab436e8342f33d52f42748820f20c2f3ced
yang-yc-squirrel <2109270989@qq.com>
2021-06-21 11:03:34 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -1,21 +1,16 @@
 import matplotlib.pyplot
 import numpy
 import pandas
+import random
+import functools
 
-d=dict(a=[1,2,3],b=[5,6,7])
-p=pandas.DataFrame(d,index=list(range(3)),columns=["b","a"])
-print(p)
-p=p.append([{"b":12,"a":108}],ignore_index=True)
-print(p)
-
-
-a=numpy.random.randint(2,15,size=(5,6))
+a=[random.randint(0,15) for i in range(10)]
 print(a)
-k=a.sum(axis=0)
-print(k)
+b=functools.reduce(lambda x,y:x+y,a)
+print(b)
 
-p1=pandas.DataFrame(a,columns=list("abcdef"))
-print(p1)
 
-ps=pandas.concat([p,p1],axis=0,join="outer")
-print(ps)
+s=0
+for i in a:
+    s+=i
+print("{0}".format(s))
