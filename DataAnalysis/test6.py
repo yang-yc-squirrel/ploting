31885e96854a545fd0756580d9698156c4b154a0
yang-yc-squirrel <2109270989@qq.com>
2021-06-24 22:07:04 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -6,8 +6,11 @@ import functools
 import itertools
 import json
 import os
+import collections
 
 a=numpy.array([random.choice(list(range(3))) for i in range(15)])
 print(a)
-b=list(range(10))
-print(b)
+d=dict(collections.Counter(a))
+print(d)
+fau=len(a)-d.get(0)
+print("{fau}".format(fau=fau))
