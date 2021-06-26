9477b9f90867b904f24a6cce461d1ebe15954d27
yang-yc-squirrel <2109270989@qq.com>
2021-06-26 09:38:01 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -8,9 +8,17 @@ import json
 import os
 import collections
 
-a=numpy.array([random.choice(list(range(3))) for i in range(15)])
+b=random.sample(list(range(10,1,-1)),5)
+a=numpy.array([random.choice(list(range(3))) for i in range(20)])
 print(a)
+print(b)
 d=dict(collections.Counter(a))
 print(d)
 fau=len(a)-d.get(0)
-print("{fau}".format(fau=fau))
+print(fau)
+ras=fau/len(a)
+print(ras)
+ras=format(ras,"0.5%")
+print("{0:->20}".format("new"))
+print(ras)
+print(type(ras))
