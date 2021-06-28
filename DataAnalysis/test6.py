69a383acda77dac03881cf10361b5ba1c10eb978
yang-yc-squirrel <2109270989@qq.com>
2021-06-28 09:52:04 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -9,23 +9,12 @@ import os
 import collections
 import docx
 
-b=random.sample(list(range(10,1,-1)),5)
 a=numpy.array([random.choice(list(range(3))) for i in range(20)])
 print(a)
-print(b)
 d=dict(collections.Counter(a))
 print(d)
-fau=len(a)-d.get(0)
-print(fau)
-ras=fau/len(a)
-print(ras)
-ras=format(ras,"0.5%")
-print("{0:->20}".format("new"))
-print(ras)
-print(type(ras))
-
-
-path="F:/abc.docx"
-f1=docx.Document()
-f1.add_paragraph("the answer is:"+ras)
-f1.save(path)
+nz=len(a)-d.get(0)
+print(nz)
+ra=nz/len(a)
+ra=format(ra,"0.6%")
+print(ra)
