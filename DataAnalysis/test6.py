4e8a703708c7bfbd42b7bbb610af9ec3adbfbf67
yang-yc-squirrel <2109270989@qq.com>
2021-07-04 16:36:05 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -10,19 +10,5 @@ import collections
 import docx
 import heapq
 
-a=collections.deque(maxlen=10)
-for i in range(20):
-    a.appendleft(i)
-print(a)
-
-b=[random.randint(0,20) for i in range(10)]
-print(b)
-
-heapq.heapify(b)
-print(b)
-c=heapq.heapreplace(b,100)
-print(c)
-print(b)
-
-heapq.heappush(b,1)
-print(b)
+n=eval(input("please inputy a number:"))
+print("{0} {1}".format(n,type(n)))
