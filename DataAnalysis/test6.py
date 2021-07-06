1aaa6420a778eb306f7195992e690b8dc943fdbb
yang-yc-squirrel <2109270989@qq.com>
2021-07-06 10:51:51 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -11,22 +11,10 @@ import docx
 import turtle
 import heapq
 
-turtle.screensize(800,600,"blue")
-
-turtle.pensize(2)
-turtle.pencolor("red")
-turtle.speed(5)
-
-turtle.forward(50)
-
-turtle.left(90)
-turtle.forward(150)
-turtle.penup()
-turtle.goto(-50,-50)
-turtle.pendown()
-
-turtle.circle(100,extent=270)
-
-turtle.home()
-
-turtle.done()
+a=numpy.array([random.choice(list(range(50))) for i in range(30)])
+a=a.reshape(5,6)
+print(a)
+p=pandas.DataFrame(a)
+print(p)
+p=p.set_index([1],drop=True)
+print(p)
