9f7cd737fe9df9e130604b4e7d7e35ef0e938edd
yang-yc-squirrel <2109270989@qq.com>
2021-07-07 17:40:48 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -16,5 +16,12 @@ a=list(range(20))
 print(a)
 a_new=list(map(lambda x:2*x-25,a))
 print(a_new)
-s=numpy.array([random.choice(a_new) for _ in range(45)]).reshape(-1,5)
+s=numpy.array([random.choice(a_new) for _ in range(15)]).reshape(-1,5)
 print(s)
+d=pandas.DataFrame(s,columns=[chr(ord("a")+i) for i in range(5,10,1)],index=list(range(12,15,1)))
+print(d)
+
+z=dict(zip([chr(ord("a")+i) for i in range(5,10,1)],[random.randint(100,200) for _ in range(5)]))
+d=d.append([z],ignore_index=True)
+print(d)
+
