3d86fdcd0286f757f44b21f3f2f9626766c2806d
yang-yc-squirrel <2109270989@qq.com>
2021-07-08 18:12:01 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -12,22 +12,25 @@ import turtle
 import heapq
 
 
-a=list(range(20))
-print(a)
-a_new=list(map(lambda x:2*x-25,a))
-print(a_new)
-s=numpy.array([random.choice(a_new) for _ in range(15)]).reshape(-1,5)
-print(s)
-d=pandas.DataFrame(s,columns=[chr(ord("a")+i) for i in range(5,10,1)],index=list(range(12,15,1)))
-print(d)
+def fei(n):
+    a=1
+    b=1
+    i=1
+    while(i<=n):
+        yield a
+        a,b=b,a+b
+        i+=1
 
-z=dict(zip([chr(ord("a")+i) for i in range(5,10,1)],[random.randint(100,200) for _ in range(5)]))
-d=d.append([z],ignore_index=True)
-print(d)
 
-g=numpy.random.randint(20,30,size=(6,3))
-print(g)
-h=pandas.DataFrame(g,columns=list("fhj"))
-print(h)
-k=pandas.concat([d,h],axis=0,join="outer")
-print(k)
+g=fei(21)
+r=[]
+while(True):
+    try:
+        new_e=next(g)
+        r.append(new_e)
+    except StopIteration:
+        print("end".center(20,"-"))
+        break
+print(r)
+
+
