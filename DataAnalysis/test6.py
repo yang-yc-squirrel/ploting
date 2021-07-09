01c6ed594a1787e368b434b424cdc2b2b526c7a6
yang-yc-squirrel <2109270989@qq.com>
2021-07-09 18:36:49 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -18,4 +18,12 @@ p=p.reshape(8,-1)
 print(p)
 o=pandas.DataFrame(p,columns=list("abc"),index=[random.choice([chr(ord("a")+i) for i in range(26)]) for _ in range(8)])
 print(o)
-o.to_csv("F:/o.csv",encoding="utf-8",header=0,index=0)
+
+a=list(o.iloc[:,0])
+print(a)
+b=[random.randint(0,20) for i in range(len(a))]
+print(b)
+b.append(100)
+print(b)
+df=dict(itertools.zip_longest(b,a,fillvalue="k"))
+print(df)
