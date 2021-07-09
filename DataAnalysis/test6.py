d1fc16ade2abaf2c9e20f7878726ae80aaf730a8
yang-yc-squirrel <2109270989@qq.com>
2021-07-09 18:31:51 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -12,52 +12,10 @@ import turtle
 import heapq
 
 
-def fei(n):
-    a=1
-    b=1
-    i=1
-    while(i<=n):
-        yield a
-        a,b=b,a+b
-        i+=1
-
-
-g=fei(21)
-r=[]
-while(True):
-    try:
-        new_e=next(g)
-        r.append(new_e)
-    except StopIteration:
-        print("end".center(20,"-"))
-        break
-print(r)
-
-r=numpy.array(r).reshape(-1,3)
-print(r)
-jk=r.sum(axis=0).reshape(-1,3)
-print(jk)
-r=numpy.r_[r,jk]
-print(format("sum","+>30"))
-print(r)
-p=pandas.DataFrame(r,columns=[chr(ord("a")+i) for i in range(3)])
+p=numpy.linspace(0,10,24)
 print(p)
-print("{0:*<30}".format("next"))
-p=p.set_index(["b"],drop=True)
+p=p.reshape(8,-1)
 print(p)
-
-tx=list(p.iloc[:,0])
-print(tx)
-print("\n")
-for i,j in enumerate(tx,5):
-    print("{i} {j}".format(i=i,j=j))
-
-for k in itertools.dropwhile(lambda x:x<200,tx):
-    print(k)
-
-print("\n")
-print("{0:+^50}".format("test"))
-yu=numpy.linspace(0,1,300)
-print(yu)
-l=len(yu)
-print(l)
+o=pandas.DataFrame(p,columns=list("abc"),index=[random.choice([chr(ord("a")+i) for i in range(26)]) for _ in range(8)])
+print(o)
+o.to_csv("F:/o.csv",encoding="utf-8",header=0,index=0)
