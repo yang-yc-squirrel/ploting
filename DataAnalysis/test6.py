56c5df1b42aa37f9eff2d1c319d3d496e53f3356
yang-yc-squirrel <2109270989@qq.com>
2021-07-23 10:58:32 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -116,3 +116,7 @@ a=[random.choice(list(range(3))) for _ in range(20)]
 print(a)
 ak=[99 if operator.eq(i,0) else i for i in a]
 print(ak)
+
+print(divmod(len(ak),3)[0])
+bk=[ak[i:i+3:1] for i in range(0,len(ak),3)]
+print(bk)
