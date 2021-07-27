568b5becbd247e5a6f0441ea1c6e40cf3e9fd6a5
yang-yc-squirrel <2109270989@qq.com>
2021-07-27 17:45:22 +0800

    distribution

--- DataAnalysis/MathematicalDistribution.py
+++ DataAnalysis/MathematicalDistribution.py
@@ -1,17 +1,14 @@
 import numpy
 import matplotlib.pyplot
+import matplotlib.animation
 
 def cauchy(x,t,s):
     fx=s*numpy.pi*(1+((x-t)/s)**2)
     fx=1/fx
     return fx
 
-x=numpy.linspace(-10,10,1000)
-fx=numpy.zeros(shape=(1,1000)).flatten()
-for i in range(1000):
-    fx[i]=cauchy(x[i],0,1)
-
 p=matplotlib.pyplot.figure()
-matplotlib.pyplot.plot(x,fx,label="f(x)=cauchy(x)")
-matplotlib.pyplot.legend(loc=1)
-matplotlib.pyplot.show()
+ax=matplotlib.pyplot.axes(xlim=(-10,10),ylim=(0,1))
+f,=ax.plot([],[])
+x=[]
+y=[]
