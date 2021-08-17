9602631fc893325858ba7acc64bdb9447ba9a084
yang-yc-squirrel <2109270989@qq.com>
2021-08-17 10:16:55 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -32,14 +32,10 @@ f=matplotlib.pyplot.figure()
 f3d=mpl_toolkits.mplot3d.Axes3D(f)
 
 a=0
-while(True):
-    matplotlib.pyplot.cla()
-    f3d.plot3D(x,y,z,color=(1,0,0))
-    f3d.view_init(a,90)
-    matplotlib.pyplot.pause(2)
-    a+=5
-    if(a>=90):
-        break
+f3d.plot(x,y,z,color=(1,0,0))
+for i in range(0,90,10):
+    f3d.view_init(i,20)
+    matplotlib.pyplot.show()
 
 
 ysi=numpy.argsort(y)
