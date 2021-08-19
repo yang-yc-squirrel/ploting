7c008fb099a3743fb721ec83a0cb0fef0b80d3fa
yang-yc-squirrel <2109270989@qq.com>
2021-08-19 21:04:16 +0800

    test

--- DataAnalysis/test6.py
+++ DataAnalysis/test6.py
@@ -21,26 +21,16 @@ def fx(x):
     z=25*numpy.cos(20*numpy.pi*x)+2*numpy.sin(5*numpy.pi*x)+15*numpy.cos(80*numpy.pi*x)
     return y,z
 
-x=numpy.linspace(0,1,1024)
-y=numpy.zeros(shape=(1,1024)).flatten()
-z=numpy.zeros(shape=(1,1024)).flatten()
 
-for i in range(1024):
-    y[i],z[i]=fx(x[i])
+def F(x):
+    d=len(x)
+    s=0
+    for i in range(d):
+        s+=x[i]*numpy.sin((abs(x[i]))**0.5)
+    s=s*(-1)+418.9829*d
+    return s
 
-f=matplotlib.pyplot.figure()
-f3d=mpl_toolkits.mplot3d.Axes3D(f)
-
-f3d.scatter3D(x,y,z,color="#ff0000",alpha=0.5)
-
-p=cv2.imread("F:/figure.jpg")
-p1=cv2.pyrDown(p)
-p2=cv2.pyrDown(p1)
-
-cv2.imshow("p",p)
-cv2.imshow("p1",p1)
-cv2.imshow("p2",p2)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-print(p1)
+p=numpy.linspace(0,500,2000)
+print(p)
+x,y=numpy.meshgrid(p,p)
+print(x)
