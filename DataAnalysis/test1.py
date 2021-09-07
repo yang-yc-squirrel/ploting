ef05de889bbc0468a53648636b38d9fbd82310eb
yang-yc-squirrel <2109270989@qq.com>
2021-09-07 19:56:33 +0800

    test

--- DataAnalysis/test1.py
+++ DataAnalysis/test1.py
@@ -1,3 +1,7 @@
+import numpy
+import random
+import matplotlib.pyplot
+
 def fib(n):
     a=1
     b=1
@@ -8,7 +12,7 @@ def fib(n):
             break
         n-=1
 
-g=fib(10)
+'''g=fib(10)
 s=0
 while(True):
     try:
@@ -18,5 +22,16 @@ while(True):
     except StopIteration:
         break
 print("end".center(50,"-"))
-print("{na}".format(na=s))
+print("{na}".format(na=s))'''
 
+
+f=matplotlib.pyplot.figure()
+
+
+ax1=matplotlib.pyplot.subplot2grid((3,3),(0,0),colspan=2,rowspan=1)
+a=list(range(1,6,1))
+b=[random.randint(5,15) for i in range(5)]
+ax1.bar(a,b,color="#ff0000")
+
+
+matplotlib.pyplot.show()
