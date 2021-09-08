8e9ed2940133d5c6ca959218fa6ac49362145209
yang-yc-squirrel <2109270989@qq.com>
2021-09-08 20:58:13 +0800

    test

--- DataAnalysis/test1.py
+++ DataAnalysis/test1.py
@@ -41,5 +41,14 @@ for i in range(100):
 ax2.plot(x,y,color="#ff0000")
 matplotlib.pyplot.tight_layout()
 
+ax3=matplotlib.pyplot.subplot2grid((3,3),(1,1),colspan=1,rowspan=1,projection="polar")
+th=numpy.arange(0,numpy.pi,numpy.pi/6)
+r=numpy.array([random.randint(1,6) for i in range(6)])
+ws=[random.randint(2,5)*numpy.pi/30 for i in range(6)]
+ax3.bar(th,r,width=ws)
+ax3.set_rlim([0,10])
+ax3.set_rticks(numpy.arange(0,3,0.1))
+ax3.set_rlabel_position(300)
+
 
 matplotlib.pyplot.show()
